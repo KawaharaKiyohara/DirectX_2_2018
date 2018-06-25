fa8477394a0dc343a7e9832dcf07e06cf04c2000
TakayukiKiyohara <nezumimusume30@gmail.com>
2018-06-25 23:07:17 +0900

    Levelを追加

---
+++ Lesson_09/Lesson_09/Player.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "character/CharacterController.h"
+
+class Player
+{
+public:
+	Player();
+	~Player();
+	void Update();
+	void Draw();
+	void SetPosition(const CVector3& pos)
+	{
+		m_position = pos;
+	}
+	
+private:
+	SkinModel m_model;									//スキンモデル。
+	Animation m_animation;								//アニメーション。
+	AnimationClip m_animationClips[2];					//アニメーションクリップ。
+	CVector3 m_position = CVector3::Zero();				//座標。
+	CVector3 m_scale = CVector3::One();					//拡大率。
+	CVector3 m_moveSpeed = CVector3::Zero();			//移動速度。
+	CQuaternion m_rotation = CQuaternion::Identity();	//回転。
+	//Hands-On 3 キャラクターコントローラーを追加。
+	CharacterController m_charaCon;
+};
+
